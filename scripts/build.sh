f9efec0d7e6c8dd804b9719820c40f7575079688
mishamyrt <misha@myrt.co>
2019-10-09 23:23:08 +0300

    Recreate folders on build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-rm -rf build/otf/*
+rm -rf build/
+mkdir build/
+mkdir build/otf
 fontforge -lang=py -script src/ligaturize.py 2>&1 | fgrep -v 'This contextual rule applies no lookups.'
