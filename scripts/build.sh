e9b32356b3cab6b98474c310ffec7625e09104c5
mishamyrt <misha@myrt.co>
2019-10-10 22:26:19 +0300

    Renamed build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,5 @@
 rm -rf build/
 mkdir build/
 mkdir build/otf
-fontforge -lang=py -script src/ligaturize.py 2>&1 | fgrep -v 'This contextual rule applies no lookups.'
+fontforge -lang=py -script src/build.py 2>&1 | fgrep -v 'This contextual rule applies no lookups.'
 python3 src/fix-meta.py
