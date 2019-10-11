8c2eddabb81aa8003b2587bd169bcda14c974c36
mishamyrt <misha@myrt.co>
2019-10-11 09:35:13 +0300

    Added TTF to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,5 +2,17 @@
 rm -rf build/
 mkdir build/
 mkdir build/otf
+mkdir build/ttf-not_hinted
+mkdir build/ttf
 fontforge -lang=py -script src/build.py 2>&1 | fgrep -v 'This contextual rule applies no lookups.'
 python3 src/fix-meta.py
+python3 src/convert.py
+
+for input_file in build/ttf-not_hinted/*.ttf
+do
+	output_file=build/ttf/$(basename "$input_file")
+    ttfautohint -I $input_file $output_file --stem-width-mode nnn --composites
+    gftools fix-dsig --autofix $output_file
+done
+
+rm -rf build/ttf-not_hinted
