b0f9bf8b7f30a50837e8944740ab2c380525faf9
mishamyrt <misha@myrt.co>
2020-11-24 15:50:07 +0300

    Generate feature name

--- generator/fea.py
+++ generator/fea.py
@@ -1,13 +1,36 @@
+
 '''
 fea files helpers
 '''
+
+from re import search, M
+
+ignore_name = [
+    'calt',
+    'mark',
+    'aalt',
+    'frac'
+]
 
 def read_file (path: str) -> str:
     with open(path) as file:
         return file.read()
 
+def get_feature_name (feature: str) -> str:
+    result = search(r'Name:(.*)', feature)
+    name = result.group(1).strip()
+    return (
+        'featureNames {\n'
+        f'name 3 1 0x0409 "{name}";\n'
+        f'name 1 0 0 "{name}";\n'
+        '};\n'
+    )
+
 def read_feature (name: str) -> str:
-    return read_file(f'./features/{name}.fea')
+    content = read_file(f'./features/{name}.fea')
+    if name.split('/')[0] in ignore_name:
+        return content
+    return get_feature_name(content) + content
 
 def read_class (name: str) -> str:
     return read_file(f'./classes/{name}.fea')
