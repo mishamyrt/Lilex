69918e00fdb933b3933ff9639227e979ed1a56de
mishamyrt <misha@myrt.co>
2023-03-27 03:15:48 +0300

    Ignore hidden files

--- scripts/utils/files.py
+++ scripts/utils/files.py
@@ -1,11 +1,12 @@
+"""File utils"""
 from os import listdir
 from os.path import isfile, join
 from typing import List
 
-def list_files(path: str) -> List[str]:
+def list_files(dir_path: str) -> List[str]:
     files = []
-    for f in listdir(path):
-        p = join(path, f)
-        if isfile(p):
-            files.append(p)
+    for file in listdir(dir_path):
+        file_path = join(dir_path, file)
+        if isfile(file_path) and not file_path.startswith('.'):
+            files.append(file_path)
     return files
