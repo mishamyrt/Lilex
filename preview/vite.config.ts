3572e29bfe21d5ace90f242b38e8054f86237d18
mishamyrt <misha@myrt.co>
2023-04-02 15:22:12 +0300

    Fix preview basedir

--- preview/vite.config.ts
+++ preview/vite.config.ts
@@ -1,25 +1,26 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { execSync } from 'child_process'
-import { viteSingleFile } from "vite-plugin-singlefile"
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 const FONTS_SOURCE = '../build'
 const FONTS_TARGET = './public'
 
-function lnFontDir(format: string) {
-    const sourcePath = `${FONTS_SOURCE}/${format}`
-    const targetPath = `${FONTS_TARGET}/${format}`
+function lnFontDir (format: string) {
+  const sourcePath = `${FONTS_SOURCE}/${format}`
+  const targetPath = `${FONTS_TARGET}/${format}`
 
-    execSync(`rm -rf "${targetPath}"`)
-    execSync(`mkdir "${targetPath}"`)
-    execSync(`ln "${sourcePath}"/* "${targetPath}"`)
+  execSync(`rm -rf "${targetPath}"`)
+  execSync(`mkdir "${targetPath}"`)
+  execSync(`ln "${sourcePath}"/* "${targetPath}"`)
 }
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-    lnFontDir('otf')
-    lnFontDir('variable')
-    return {
-        plugins: [svelte(), viteSingleFile()],
-    }
+  lnFontDir('otf')
+  lnFontDir('variable')
+  return {
+    base: '',
+    plugins: [svelte(), viteSingleFile()],
+  }
 })
