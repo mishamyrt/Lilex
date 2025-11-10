7eed2592f1a02793b21312b472201155bb21900b
mishamyrt <misha@myrt.co>
2025-11-10 23:54:45 +0300

    chore: add typograf

--- preview/astro.config.mjs
+++ preview/astro.config.mjs
@@ -1,11 +1,21 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import { fileLinker } from "./file-linker.mjs";
+import typograf from "astro-typograf";
 
 const FONTS_SOURCE = "../build/webfonts";
 const FONTS_TARGET = "./public/fonts";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [fileLinker(FONTS_SOURCE, FONTS_TARGET)],
+  integrations: [
+    fileLinker(FONTS_SOURCE, FONTS_TARGET),
+    typograf({
+      selector: "p",
+      typografOptions: {
+        locale: ["en-US"],
+        htmlEntity: { type: "name" },
+      },
+    }),
+  ],
 });
