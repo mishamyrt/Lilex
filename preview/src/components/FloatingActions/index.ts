d3c518031550f2f0f3660aa29d094f46dfbe684a
mishamyrt <misha@myrt.co>
2025-11-11 01:06:32 +0300

    feat(preview): improve mobile ux

---
+++ preview/src/components/FloatingActions/index.ts
@@ -0,0 +1,5 @@
+import FloatingPanel from "./FloatingPanel.astro";
+import FloatingAction from "./FloatingAction.astro";
+import FloatingToggle from "./FloatingToggle.astro";
+
+export { FloatingPanel, FloatingAction, FloatingToggle };
