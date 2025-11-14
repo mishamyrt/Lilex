db31adb9c6099e9b394f8df6006c8d40eb6d2676
mishamyrt <misha@myrt.co>
2025-11-14 22:48:03 +0300

    refactor(website): use file paths for linker

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,13 +3,17 @@ import { defineConfig } from "astro/config";
 import { fileLinker } from "./file-linker.mjs";
 import typograf from "astro-typograf";
 
-const FONTS_SOURCE = "../build/Lilex/webfonts";
-const FONTS_TARGET = "./public/fonts";
+const BUILD_DIR = "../build";
 
+const FONTS_SOURCES = [
+    `${BUILD_DIR}/LilexDuo/webfonts/LilexDuo\\[wght\\].woff2`,
+    `${BUILD_DIR}/Lilex/webfonts/Lilex*\\[wght\\].woff2`,
+];
+const FONTS_TARGET = "./public/fonts";
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    fileLinker(FONTS_SOURCE, FONTS_TARGET),
+    fileLinker(FONTS_SOURCES, FONTS_TARGET),
     typograf({
       selector: "p",
       typografOptions: {
